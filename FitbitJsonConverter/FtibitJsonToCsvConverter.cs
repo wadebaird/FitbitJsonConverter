472158ce357004f3e92750ed9a5edc6ef1112807
Wade Baird <wadebaird@hotmail.com>
2022-01-17 14:47:47 -0700

    Modified the code to split the output csv into smaller chunks that seem to import more easily into garmin connect.

--- FitbitJsonConverter/FtibitJsonToCsvConverter.cs
+++ FitbitJsonConverter/FtibitJsonToCsvConverter.cs
@@ -16,6 +16,14 @@ namespace Sunbreak.FitbitJsonConverter
         {
             this.inputFolderName = inputFolderName;
 
+            var outputFileNameExtension = Path.GetExtension(outputFileName);
+            if(string.IsNullOrEmpty(outputFileNameExtension))
+            {
+                outputFileName = ".csv";
+            }
+
+            outputFileName = Path.Combine(Path.GetDirectoryName(outputFileName), Path.GetFileNameWithoutExtension(outputFileName));
+
             var earliestDateTime = DateTime.Today;
             var altitudes = ReadFiles<Altitude, int>("altitude", false, ref earliestDateTime);
             var calories = ReadFiles<Calories, decimal>("calories", false, ref earliestDateTime);
@@ -28,48 +36,81 @@ namespace Sunbreak.FitbitJsonConverter
             var sleeps = ReadFiles<Sleep>("sleep").OrderBy(x => x.StartTime);
             var weights = ReadFiles<WeightData>("weight").OrderBy(x => x.Date);
 
-            using StreamWriter writer = File.CreateText(outputFileName);
+            int i = 1;
+            int counter = 0;
+            StreamWriter writer = null;
 
-            await writer.WriteLineAsync("Body");
-            await writer.WriteLineAsync("Date,Weight,BMI,Fat");
+            async Task CreateWriter(string headerName, string columnHeaders, string filePostFix)
+            {
+                if (null != writer)
+                {
+                    writer.Close();
+                }
+
+                counter = 0;
+                writer = File.CreateText($"{outputFileName}-{headerName}{filePostFix}-{i++}{outputFileNameExtension}");
+                await writer.WriteLineAsync(headerName);
+                await writer.WriteLineAsync(columnHeaders);
+            }
+
+            async Task ResetWriter(string headerName, string columnHeaders, string filePostFix = null)
+            {
+                i = 1;
+                await CreateWriter(headerName, columnHeaders, filePostFix);
+            }
+
+            async Task CheckToSplitWriter(string headerName, string columnHeaders, string filePostFix = null)
+            {
+                if (counter++ > 184)
+                {
+                    await CreateWriter(headerName, columnHeaders, filePostFix);
+                }
+            }
+
+            string bodyHeaderName = "Body";
+            string bodyColumnHeaders = "Date,Weight,BMI,Fat";
+            await ResetWriter(bodyHeaderName, bodyColumnHeaders);
 
             foreach (var weight in weights)
             {
                 await writer.WriteLineAsync($"\"{weight.Date:yyyy-MM-dd}\",\"{weight.Weight:0.0}\",\"{weight.Bmi:0.0}\",\"{weight.Fat:#,##0}\"");
+
+                await CheckToSplitWriter(bodyHeaderName, bodyColumnHeaders);
             }
 
-            await writer.WriteLineAsync();
-            await writer.WriteLineAsync("Activities");
-            await writer.WriteLineAsync("Date,Calories Burned,Steps,Distance,Floors,Minutes Sedentary,Minutes Lightly Active,Minutes Fairly Active,Minutes Very Active,Activity Calories");
+            string activitiesHeaderName = "Activities";
+            string activitiesColumnHeaders = "Date,Calories Burned,Steps,Distance,Floors,Minutes Sedentary,Minutes Lightly Active,Minutes Fairly Active,Minutes Very Active,Activity Calories";
+            await ResetWriter(activitiesHeaderName, activitiesColumnHeaders, $"-{earliestDateTime.Year}");
 
-
-            for (DateTime i = earliestDateTime; i < DateTime.Today; i = i.AddDays(1))
+            for (DateTime date = earliestDateTime; date < DateTime.Today; date = date.AddDays(1))
             {
-                if (altitudes.TryGetValue(i, out int floors))
+                if (altitudes.TryGetValue(date, out int floors))
                 {
                     floors /= 10; //Convert feet to floors
                 }
 
-                calories.TryGetValue(i, out decimal calorie);
-                steps.TryGetValue(i, out int step);
+                calories.TryGetValue(date, out decimal calorie);
+                steps.TryGetValue(date, out int step);
 
-                if (distances.TryGetValue(i, out int distance))
+                if (distances.TryGetValue(date, out int distance))
                 {
                     distance /= 160400; //Average number from monthly vs archived data. No clue what this means.
                 }
 
-                lightlyActiveMinutes.TryGetValue(i, out int lightlyActiveMin);
-                moderatelyActiveMinutes.TryGetValue(i, out int moderatelyActiveMin);
-                sedentaryMinutes.TryGetValue(i, out int sedentaryMin);
-                veryActiveMinutes.TryGetValue(i, out int veryActiveMin);
+                lightlyActiveMinutes.TryGetValue(date, out int lightlyActiveMin);
+                moderatelyActiveMinutes.TryGetValue(date, out int moderatelyActiveMin);
+                sedentaryMinutes.TryGetValue(date, out int sedentaryMin);
+                veryActiveMinutes.TryGetValue(date, out int veryActiveMin);
 
                 //                                                                                                                                                                                                                                           -1300 is just an average extrapolated from the archive data vs last month data
-                await writer.WriteLineAsync($"\"{i:yyyy-MM-dd}\",\"{calorie:#,##0}\",\"{step:#,##0}\",\"{distance}\",\"{floors:#,##0}\",\"{sedentaryMin:#,##0}\",\"{lightlyActiveMin:#,##0}\",\"{moderatelyActiveMin:#,##0}\",\"{veryActiveMin:#,##0}\",\"{(calorie - 1300):#,##0}\"");
+                await writer.WriteLineAsync($"\"{date:yyyy-MM-dd}\",\"{calorie:#,##0}\",\"{step:#,##0}\",\"{distance}\",\"{floors:#,##0}\",\"{sedentaryMin:#,##0}\",\"{lightlyActiveMin:#,##0}\",\"{moderatelyActiveMin:#,##0}\",\"{veryActiveMin:#,##0}\",\"{(calorie - 1300):#,##0}\"");
+
+                await CheckToSplitWriter(activitiesHeaderName, activitiesColumnHeaders, $"-{date.Year}");
             }
 
-            await writer.WriteLineAsync();
-            await writer.WriteLineAsync("Sleep");
-            await writer.WriteLineAsync("Start Time,End Time, Minutes Asleep,Minutes Awake, Number of Awakenings, Time in Bed,Minutes REM Sleep,Minutes Light Sleep,Minutes Deep Sleep");
+            string sleepHeaderName = "Sleep";
+            string sleepColumnNames = "Start Time,End Time, Minutes Asleep,Minutes Awake, Number of Awakenings, Time in Bed,Minutes REM Sleep,Minutes Light Sleep,Minutes Deep Sleep";
+            await ResetWriter(sleepHeaderName, sleepColumnNames);
 
             foreach (var sleep in sleeps)
             {
@@ -84,6 +125,8 @@ namespace Sunbreak.FitbitJsonConverter
                 }
 
                 await writer.WriteLineAsync($"\"{sleep.StartTime:yyyy-MM-dd h:mm tt}\",\"{sleep.EndTime:yyyy-MM-dd h:mm tt}\",\"{sleep.MinutesAsleep:#,##0}\",\"{sleep.MinutesAwake:#,##0}\",\"{numberOfAwakenings:#,##0}\",\"{sleep.TimeInBed:#,##0}\",\"{(sleep.Levels.Summary.Rem != null ? sleep.Levels.Summary.Rem.Minutes : "N/A")}\",\"{(sleep.Levels.Summary.Light != null ? sleep.Levels.Summary.Light.Minutes : "N/A")}\",\"{(sleep.Levels.Summary.Deep != null ? sleep.Levels.Summary.Deep.Minutes : "N/A")}\"");
+
+                await CheckToSplitWriter(sleepHeaderName, sleepColumnNames);
             }
         }
 
